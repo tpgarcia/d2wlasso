f1ef80d30edc95504fe951662b9b77b0e4836ac3
rakheon <rakheon.kim@gmail.com>
2019-08-05 16:48:05 -0500

    initialize the project

---
+++ R/d2wlasso.R
@@ -0,0 +1,8 @@
+rm(list=ls(all=TRUE))
+library(lars)
+source("/Users/junkyungkim/Desktop/Researchwork/R_documentation/Microbe/qw_main_functions.R")
+
+d2wlasso <- function(x,z,y){
+    print(x)
+    return(x)
+}
