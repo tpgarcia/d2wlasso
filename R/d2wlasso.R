4186e5d1a5a2d0c69d4f8a1f7665c4ebbc729966
rakheon <rakheon.kim@gmail.com>
2019-08-05 21:53:50 -0500

    added initial documentation for d2wlasso

--- R/d2wlasso.R
+++ R/d2wlasso.R
@@ -1,8 +1,19 @@
-rm(list=ls(all=TRUE))
-library(lars)
-source("/Users/junkyungkim/Desktop/Researchwork/R_documentation/Microbe/qw_main_functions.R")
-
+#' Title
+#'
+#' @param x (n by m1) matrix of main covariates
+#' @param z (n by m0) matrix of additional fixed covariates affecting response variable
+#' @param y vector of response variable
+#'
+#' @return sumx: sum of main covariates
+#' @return sumz: sum of additional fixed covariates
+#' @export
+#'
+#' @examples
+#' x=matrix(rnorm(100*5, 0, 1),100,5)
+#' z=matrix(rnorm(100*1, 0, 1),100,1)
+#' y=2*x[,1] - (2+2*z[,1])*x[,2] + rnorm(100, 0, 1)
+#' d2wlasso(x,z,y)
 d2wlasso <- function(x,z,y){
     print(x)
-    return(x)
+    return(list("sumx"=sum(x), "sumz"=sum(z)))
 }
