8666be35b5a85c482f76890b96ade3840c9d3ada
rakheon <rakheon.kim@gmail.com>
2019-08-06 00:12:56 -0500

    added correlation function

--- R/d2wlasso.R
+++ R/d2wlasso.R
@@ -1,19 +1,34 @@
 #' Implement structured variables selection with q-values
 #'
-#' @param x (n by m1) matrix of main covariates
-#' @param z (n by m0) matrix of additional fixed covariates affecting response variable
-#' @param y vector of response variable
+#' @param x (n by m1) matrix of main covariates where m1 is the number of covariates and n is the sample size
+#' @param z (n by 1) matrix of additional fixed covariate affecting response variable
+#' @param y (n by 1) matrix of response variable
 #'
-#' @return sumx: sum of main covariates
-#' @return sumz: sum of additional fixed covariates
+#' @return cor.out
+#' @return parcor.out
 #' @export
 #'
 #' @examples
 #' x=matrix(rnorm(100*5, 0, 1),100,5)
 #' z=matrix(rnorm(100*1, 0, 1),100,1)
-#' y=2*x[,1] - (2+2*z[,1])*x[,2] + rnorm(100, 0, 1)
+#' y=matrix(z[1,] + 2*x[1,] - 2*x[2,] + rnorm(100, 0, 1), 100)
 #' d2wlasso(x,z,y)
 d2wlasso <- function(x,z,y){
-    print(x)
-    return(list("sumx"=sum(x), "sumz"=sum(z)))
+
+    # dimension setting
+    n <- nrow(x)
+    m0 <- ncol(z)
+    m1 <- ncol(x)
+
+    # arranging input data
+    X <- cbind(z, x)
+    colnames(X) <- c("Diet",paste("X_",seq(1,m1),sep=""))
+    microbes <- t(X)
+    phenotype <- t(y)
+
+    # compute correlation and partial correlation (for taking into account z) between x and y
+    cor.out <- correlations(microbes,phenotypes,partial=FALSE,ttest=ttest)
+    parcor.out <- correlations(microbes,phenotypes,partial=TRUE,ttest=ttest)
+
+    return(list("cor.out"=cor.out, "parcor.out"=parcor.out))
 }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(d2wlasso)
+
+test_check("d2wlasso")
