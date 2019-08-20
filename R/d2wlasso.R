fdc97bb90f5d1e66937e49e40b4ec4ccc4ab9ba4
rakheon <rakheon.kim@gmail.com>
2019-08-20 15:47:04 -0500

    added output for q-val and p-val

--- R/d2wlasso.R
+++ R/d2wlasso.R
@@ -21,8 +21,14 @@
 #' @param ncv indicates the number of cross-validation runs for selecting delta.
 #'
 #'
-#' @return cor.out
-#' @return parcor.out
+#' @return qval: q-value as proposed in Storey and Tibshirani (2003)
+#' @return out.benhoch.pval.adjust: Benjamini-Hochberg adjusted p-value as proposed in Benjamini and Hochberg (1995)
+#' @return pval: p-value for each covariate
+#' @return out.cor
+#' @return out.benhoch.cor
+#' @return out.parcor
+#' @return out.benhoch
+#' @return qval
 #' @export
 #'
 #' @examples
@@ -199,5 +205,5 @@ d2wlasso <- function(x,z,y,ttest=FALSE,q_method=c("bootstrap","smoother")[2],plo
 
     }
 
-    return(list("cor.out"=cor.out, "parcor.out"=parcor.out, "qval"=out.qvalue,"out.benhoch.pval.adjust"=out.benhoch.pval.adjust, "pval"=out.pvalue, "out.cor"=out.cor, "out.benhoch.cor"=out.benhoch.cor, "out.parcor"=out.parcor, "out.benhoch"=out.benhoch, "out.w"=out.w, "alpha"=alpha, "alpha.bh"=alpha.bh, "delta"=delta, "mult.delta.w5"=mult.delta.w5, "mult.delta.w6"=mult.delta.w6, "mult.cv.delta.out.w5.summary"=mult.cv.delta.out.w5.summary, "mult.cv.delta.out.w6.summary"=mult.cv.delta.out.w6.summary, "mult.cv.delta.out.w5"=mult.cv.delta.out.w5, "mult.cv.delta.out.w6"=mult.cv.delta.out.w6))
+    return(list("qval"=out.qvalue,"out.benhoch.pval.adjust"=out.benhoch.pval.adjust, "pval"=out.pvalue, "out.cor"=out.cor, "out.benhoch.cor"=out.benhoch.cor, "out.parcor"=out.parcor, "out.benhoch"=out.benhoch, "out.w"=out.w, "alpha"=alpha, "alpha.bh"=alpha.bh, "delta"=delta, "mult.delta.w5"=mult.delta.w5, "mult.delta.w6"=mult.delta.w6, "mult.cv.delta.out.w5.summary"=mult.cv.delta.out.w5.summary, "mult.cv.delta.out.w6.summary"=mult.cv.delta.out.w6.summary, "mult.cv.delta.out.w5"=mult.cv.delta.out.w5, "mult.cv.delta.out.w6"=mult.cv.delta.out.w6))
 }
