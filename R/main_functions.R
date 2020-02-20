cd6edfe7ed1f6592c22d092683eb8f44ea737268
Tanya Garcia <37775239+tpgarcia@users.noreply.github.com>
2020-02-19 19:09:38 -0600

    Removed examples from d2wlasso.

--- R/main_functions.R
+++ R/main_functions.R
@@ -85,25 +85,6 @@
 #' x = matrix(rnorm(100*5, 0, 1),100,5)
 #' z = matrix(rbinom(100, 1, 0.5),100,1)
 #' y = matrix(z[,1] + 2*x[,1] - 2*x[,2] + rnorm(100, 0, 1), 100)
-#' dwl0 <- d2wlasso(x,z,y)
-#' dwl1 <- d2wlasso(x,z,y,delta=2)
-#' dwl2 <- d2wlasso(x,z,y,include.z=FALSE)
-#' dwl3 <- d2wlasso(x,z,y,weight_fn = "sqrt")
-#' dwl4 <- d2wlasso(x,z,y,wt="adapt")
-#' dwl5 <- d2wlasso(x,z,y,wt="t_val")
-#' dwl6 <- d2wlasso(x,z,y,wt="q_parcor")
-#' dwlcv0 <- d2wlasso(x,z,y,lasso.delta.cv.mult = TRUE, ncv = 3)
-#' dwlcv1 <- d2wlasso(x,z,y,lasso.delta.cv.mult = TRUE, ncv = 3, delta.cv.seed = 1)
-#' dwlcv2 <- d2wlasso(x,z,y,weight_fn = "square",lasso.delta.cv.mult = TRUE, ncv = 3, delta.cv.seed = 1)
-#'
-#' x <- matrix(rnorm(100*5, 0, 1),100,5)
-#' z <- matrix(rbinom(100, 1, 0.5),100,1)
-#' y <- matrix(exp(z[,1] + 2*x[,1] - 2*x[,2] + rnorm(100, 0, 2)), 100)
-#' cox.delta <- matrix(1,nrow=length(y),ncol=1)
-#' dwlcox1 <- d2wlasso(x,z,y,cox.delta = cox.delta, regression.type = "cox")
-#' dwlcox2 <- d2wlasso(x,z,y,cox.delta = cox.delta, regression.type = "cox", nboot = 50)
-#' dwlcox3 <- d2wlasso(x,z,y,cox.delta = cox.delta, regression.type = "cox", wt="t_val")
-#' dwlcoxcv1 <- d2wlasso(x,z,y,cox.delta = cox.delta,regression.type = "cox",lasso.delta.cv.mult = TRUE, ncv = 3, nboot = 50)
 d2wlasso <- function(x,z,y,cox.delta=NULL,
                      factor.z=TRUE,
                      regression.type=c("linear","cox")[1],
